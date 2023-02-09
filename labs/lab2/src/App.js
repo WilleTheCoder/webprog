ba758e355447b08c503c0b1fec86e2eb7f2d0883
Daniel Dahlén <daniel@znitz.se>
2023-02-09 05:23:53 +0100

    working options

--- labs/lab2/src/App.js
+++ labs/lab2/src/App.js
@@ -4,27 +4,21 @@ import 'bootstrap/dist/css/bootstrap.css';
 import inventory from './inventory.ES6';
 import ComposeSalad from './ComposeSalad';
 
-class App extends Component {
+function App() {
+  return (
+    <div className="container py-4">
+      <header className="pb-3 mb-4 border-bottom">
+        <span className="fs-4">Min egen salladsbar</span>
+      </header>
 
-  render() {
-    return (
-      <div className="container py-4">
-        <ComposeSalad inventory={inventory} />
+      <ComposeSalad inventory={inventory} />
 
-        <form>
-          <label>
-            Name:
-            <input type="text" name="name" />
-          </label>
-          <input type="submit" value="submit"/>
-        </form>
-        
-        <footer className="pt-3 mt-4 text-muted border-top">
-          EDAF90 - webprogrammering
-        </footer>
-      </div>
-    );
-  }
+      <footer className="pt-3 mt-4 text-muted border-top">
+        EDAF90 - webprogrammering
+      </footer>
+    </div>
+  );
 }
-
 export default App;
+
+// setExtras:setExtras( oldState => copyAndUpdate(oldStat) )
--- labs/lab2/src/ComposeSalad.js
+++ labs/lab2/src/ComposeSalad.js
@@ -1,21 +1,23 @@
-import { Component } from 'react';
-class ComposeSalad extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+import { useState } from 'react';
+import inventory from './inventory.ES6.js';
 
-    render() {
-        let extras = Object.keys(this.props.inventory)
-            .filter(name => this.props.inventory[name].extra);
-        return (
-            <div className="continer col-12">
-                <div className="row h-200 p-5 bg-light border rounded-3">
-                    <h2>Välj innehållet i din sallad</h2>
-                    {extras.map(name => <div key={name} className="col-4">{name}</div>)}
-                </div>
+function ComposeSalad(props) {
+    let extras = Object.keys(props.inventory).filter(name => props.inventory[name].foundation);
+    const [foundation, setFoundation] = useState('Pasta');
+    const [extra, setExtra] = useState({ Bacon: true, Fetaost: true });
+
+    let foundations = Object.keys(props.inventory).filter(name => props.inventory[name].foundation)
+
+    return (
+        <div className="container col-12">
+            <div className="row h-200 p-5 bg-light border rounded-3">
+                <h2>Välj bas</h2>
+                <select value={foundation} onChange={e => setFoundation(e.target.value)}>
+                    {foundations.map(name => <option key={name} value={name}>{name}</option>)}
+                </select>
+                {/* {extras.map(name => <div key={name} className="col-4">{name}</div>)} */}
             </div>
-        );
-    }
+        </div>
+    );
 }
 export default ComposeSalad;
