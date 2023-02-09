2c86ccf9e9477b4ea265b8a3e9f51dc86629b698
Daniel Dahlén <daniel@znitz.se>
2023-02-09 20:55:57 +0100

    fel på inläggning av sallader

--- labs/lab2/src/App.js
+++ labs/lab2/src/App.js
@@ -1,17 +1,26 @@
-import { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import inventory from './inventory.ES6';
 import ComposeSalad from './ComposeSalad';
+import ShoppingCart from './ShoppingCart';
+import { useState } from 'react';
 
 function App() {
+
+  const [salads, setSalads] = useState([]);
+
+  const saladSubmit = (salad) => {
+    setSalads({ ...salads, salad });
+  }
+
   return (
     <div className="container py-4">
       <header className="pb-3 mb-4 border-bottom">
         <span className="fs-4">Min egen salladsbar</span>
       </header>
 
-      <ComposeSalad inventory={inventory} />
+      <ShoppingCart cart={salads} />
+      <ComposeSalad inventory={inventory} saladSubmit={saladSubmit} />
 
       <footer className="pt-3 mt-4 text-muted border-top">
         EDAF90 - webprogrammering
@@ -20,5 +29,3 @@ function App() {
   );
 }
 export default App;
-
-// setExtras:setExtras( oldState => copyAndUpdate(oldStat) )
--- labs/lab2/src/ComposeSalad.js
+++ labs/lab2/src/ComposeSalad.js
@@ -1,23 +1,85 @@
 import { useState } from 'react';
 import inventory from './inventory.ES6.js';
+import Salad from './Salad.js';
 
 function ComposeSalad(props) {
-    let extras = Object.keys(props.inventory).filter(name => props.inventory[name].foundation);
     const [foundation, setFoundation] = useState('Pasta');
-    const [extra, setExtra] = useState({ Bacon: true, Fetaost: true });
+    const [protein, setProtein] = useState('Kycklingfilé');
+    const [dressing, setDressing] = useState('Ceasardressing');
+    const [extra, setExtra] = useState({});
 
-    let foundations = Object.keys(props.inventory).filter(name => props.inventory[name].foundation)
+    let extras = Object.keys(props.inventory).filter(name => props.inventory[name].extra);
+    let proteins = Object.keys(props.inventory).filter(name => props.inventory[name].protein);
+    let dressings = Object.keys(props.inventory).filter(name => props.inventory[name].dressing);
+    let foundations = Object.keys(props.inventory).filter(name => props.inventory[name].foundation);
+
+    const handleChange = event => {
+        const { name, checked } = event.target;
+        setExtra(oldState => {
+            const copy = { ...oldState };
+            copy[name] = checked;
+            return copy;
+        });
+    };
+
+    const handleSubmit = event => {   
+        event.preventDefault();
+        let extras = Object.keys(extra).filter((n) => extra[n]);
+        let ingredients = [foundation, protein, ...extras, dressing]
+        let salad = new Salad();
+
+        ingredients.forEach((i) => salad.add(i, inventory[i]));
+        resetForm();
+        props.saladSubmit(salad);       
+    }
+
+    const resetForm = function() {
+        setFoundation('Pasta');
+        setProtein('Kycklingfilé');
+        setDressing('Ceasardressing');
+        setExtra({});
+    }
 
     return (
         <div className="container col-12">
-            <div className="row h-200 p-5 bg-light border rounded-3">
-                <h2>Välj bas</h2>
-                <select value={foundation} onChange={e => setFoundation(e.target.value)}>
-                    {foundations.map(name => <option key={name} value={name}>{name}</option>)}
-                </select>
-                {/* {extras.map(name => <div key={name} className="col-4">{name}</div>)} */}
-            </div>
-        </div>
+            <form onSubmit={handleSubmit}>
+                <div className="row h-200 p-5 bg-light border rounded-3">
+
+                    <h2>Välj bas</h2>
+                    <div className="row pb-3">
+                        <select value={foundation} onChange={e => setFoundation(e.target.value)}>
+                            {foundations.map(name => <option key={name} value={name}>{name}</option>)}
+                        </select>
+                    </div>
+
+                    <h2>Välj protein</h2>
+                    <div className="row pb-3">
+                        <select value={protein} onChange={e => setProtein(e.target.value)}>
+                            {proteins.map(name => <option key={name} value={name}>{name}</option>)}
+                        </select>
+                    </div>
+
+                    <h2>Välj dressing</h2>
+                    <div className="row pb-3">
+                        <select value={dressing} onChange={e => setDressing(e.target.value)}>
+                            {dressings.map(name => <option key={name} value={name}>{name}</option>)}
+                        </select>
+                    </div>
+
+                    <h2>Välj tillbehör</h2>
+                    <div className="row pb-3">
+                        {extras.map((item, index) => (
+                            <div key={index} className="col-3 p-1 fs-6">
+                                <input value={item} type="checkbox" onChange={handleChange} name={item} checked={!!extra[item]} />
+                                <span> {item}</span>
+                            </div>
+                        ))}
+                    </div>
+
+                    <button type="submit" className="btn btn-primary">Lägg till</button>
+                </div>
+            </form>
+        </div >
     );
 }
 export default ComposeSalad;
