66b23140fd6d63726ce1ad752938d104be913a87
daniel <daniel@znitz.se>
2023-02-16 12:47:58 +0100

    BOP

--- labs/lab2/src/App.js
+++ labs/lab2/src/App.js
@@ -31,37 +31,3 @@ function App() {
   );
 }
 export default App;
-
-
-// class App extends Component {
-//   constructor(){
-//     super();
-//     this.state = {
-//       shoppingCart : []
-//     }
-    
-//   }
-//   render(){
-
-//      const saladSubmit = (salad) => {
-//        this.setState({shoppingCart: salad})
-//       //  setSalads([...shoppingCart, salad]);
-//      }
-
-//      return (
-//        <div className="container py-4">
-//       <header className="pb-3 mb-4 border-bottom">
-//         <span className="fs-4">Min egen salladsbar</span>
-//       </header>
-
-//       <ViewOrder shoppingCart={this.state.shoppingCart} />
-//       <ComposeSalad inventory={inventory} saladSubmit={saladSubmit} />
-
-//       <footer className="pt-3 mt-4 text-muted border-top">
-//         EDAF90 - webprogrammering
-//       </footer>
-//     </div>
-//   );
-// }
-// } 
-// export default App;
